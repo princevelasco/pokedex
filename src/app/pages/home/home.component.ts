f4746e013a2a855ca2793cdc63c811a2460b9cc7
princevelasco <prensvelasco@gmail.com>
2025-03-22 01:22:50 +0800

    update homepage add animations, pokedex update bg colors

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,37 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Router } from '@angular/router';
 
-export const fadeInOutTimeout = 250;
-export const fadeInOut = trigger('fadeInOut', [
-  transition('void => *', [style({ opacity: '0', transform: 'translateX(-10%)' }), animate(fadeInOutTimeout)]),
-  transition('* => void', [animate(fadeInOutTimeout, style({ opacity: '0' }))]),
-  transition('* => *', [
-style({ opacity: '0', transform: 'translateX(-10%)' }),
-animate(fadeInOutTimeout, style({ opacity: '1', transform: 'translateX(0%)' })),
-  ]),
-]);
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('slideLines', [
+      transition(':enter', [
+        style({ width: 0 }),
+        animate('2s', style({ width: '100%' })),
+      ]),
+    ]),
+
+    trigger('showText', [
+      state('show', style({
+        "margin-left": 0,
+        display: 'block'
+      })),
+      transition('none => show', [
+        animate('1s')
+      ]),
+    ]),
+
+    trigger('showPokemon', [
+      state('show', style({
+        opacity: 1
+      })),
+      transition('none => show', [
+        animate('1s')
+      ]),
+    ]),
+  ]
 })
+
 export class HomeComponent implements OnInit {
   column: number = 3;
-  row: string = '100%';
+  row: string = '90%';
+  show: any = {
+    text: false,
+    image: false
+  }
 
   constructor(
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-    // this.column = (window.innerWidth <= 400) ? 3 : 2;
-    // this.row = (window.innerWidth <= 400) ? '50%' : '100%';
-  }
+  ngOnInit(): void { }
 
-  onResize(event:any) {
-    // this.column = (event.target.innerWidth <= 400) ? 3 : 2;
-    // this.row = (window.innerWidth <= 400) ? '50%' : '100%';
+  animationDone(event:any, type: any) {
+    switch (type) {
+      case 'line':
+          this.show['text'] = true;
+        break;
+      case 'text':
+          this.show['image'] = event.toState == 'show' ? true : false;
+        break;
+    }
   }
 
   openPokedex() {
